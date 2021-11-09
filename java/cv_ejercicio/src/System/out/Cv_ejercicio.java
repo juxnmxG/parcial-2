3953aa851e07db69758a038df6dd3b234396f999
juxnmxG <juxnmxgxmbox@gmail.com>
2021-11-09 18:54:11 -0500

    version 1

--- java/cv_ejercicio/src/System/out/Cv_ejercicio.java
+++ java/cv_ejercicio/src/System/out/Cv_ejercicio.java
@@ -5,6 +5,7 @@ public class Cv_ejercicio {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("version 0");
+		System.out.print("version 1");
 	}
 
 }
