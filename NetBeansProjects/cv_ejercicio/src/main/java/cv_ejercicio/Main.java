ef8a4fe71cab1761f117859714b973b48670c869
juxnmxG <juxnmxgxmbox@gmail.com>
2021-11-09 20:51:30 -0500

    version 1

--- NetBeansProjects/cv_ejercicio/src/main/java/cv_ejercicio/Main.java
+++ NetBeansProjects/cv_ejercicio/src/main/java/cv_ejercicio/Main.java
@@ -16,6 +16,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("version 0");
+        System.out.print("version 1");
     }
     
 }
